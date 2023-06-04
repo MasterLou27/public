6b5adb0354dad9039bf4c1c5c5701889d979ac73
MasterLou27 <135483335+MasterLou27@users.noreply.github.com>
2023-06-03 22:37:55 -0400

    Update credscan.sql
    Actual credential scanner for bigquery hosted projects.

--- credscan.sql
+++ credscan.sql
@@ -1,39 +1,18 @@
-#legacySQL
-SELECT
-  SPLIT(Content, '\n') AS Code
-FROM (
-  
+  #legacySQL
   SELECT
-    RTRIM(LTRIM(REGEXP_EXTRACT(Code, r'(.*=.*alloc.*\(.[[:print:]][+*-]+.*)' ))) IS NOT NULL,
-    repo_name,
+    Content,
     id,
     binary
   FROM
     [bigquery-public-data:github_repos.contents]
-  WHERE
-    Code IS NOT NULL 
-    AND (binary IS FALSE)
-    AND REGEXP_MATCH(Code, r'.*\(.*sizeof\s*\(\**[[:print:]]+\.*') == FALSE
-    AND REGEXP_MATCH(Code, r'.*\(.*sizeof\s*\(\**[[:print:]]+\.*\).*\)') == FALSE) AS contentx
-JOIN (
-  SELECT
-    tstamp,
-    id,
-    path,
-    repo_name
-  FROM
-    [bigquery-public-data:github_repos.files]
-  HAVING
-    Code IS NOT NULL
-  WHERE 
-    LENGTH(path) > 4
-    AND (RIGHT (path, 4) = '.cpp'
-      OR RIGHT(path, 4) = '.hpp'
-      OR RIGHT(path, 2) = '.c'
-      OR RIGHT(path, 2) = '.h'
-      OR RIGHT(path, 4) = '.hxx'
-      OR RIGHT(path, 3) = '.cc'
-      OR RIGHT(path, 4) = '.c++' )
-    AND NOT LOWER(path) CONTAINS "test" ) AS filesx
-ON
-  contentx.id = filesx.id,
+  WHERE Content IS NOT NULL
+    //AND binary IS FALSE
+    AND (
+      REGEXP_MATCH(Content, r'.*-----BEGIN RSA PRIVATE KEY-----.*')
+      OR REGEXP_MATCH(Content, r'.*BasicAWSCredentials.*')
+      OR REGEXP_MATCH(Content, r'.*AWS.*Secret.*Key.*')
+      OR REGEXP_MATCH(Content, r'.*SecretKey.*')
+      OR REGEXP_MATCH(Content, r'.*AKIA[0-9A-Z]{16}.*')
+    )
+    IGNORE CASE
+  
